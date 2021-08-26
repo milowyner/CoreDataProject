54e170e454c0e72b4033f00a204f0d2cf5dcf491
Milo Wyner <milowyner@gmail.com>
2021-08-26 15:30:57 -0700

    Create new example showing one to many relationships using countries and candies

--- Candy+CoreDataClass.swift
+++ Candy+CoreDataClass.swift
@@ -1,5 +1,5 @@
 //
-//  Singer+CoreDataClass.swift
+//  Candy+CoreDataClass.swift
 //  CoreDataProject
 //
 //  Created by Milo Wyner on 8/26/21.
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-@objc(Singer)
-public class Singer: NSManagedObject {
+@objc(Candy)
+public class Candy: NSManagedObject {
 
 }
---
+++ Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Milo Wyner on 8/26/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
